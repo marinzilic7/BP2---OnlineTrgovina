8917bba8d62b0dbedd88149e375e10b326838a00
Contineo <marinzilic507@gmail.com>
2023-07-23 02:14:33 +0200

    final

--- model/app.py
+++ model/app.py
@@ -35,6 +35,44 @@ def dodaj_proizvod():
     return redirect(url_for('index'))
 
 
+@app.route('/izbrisi_proizvod/<int:ID_proizvoda>', methods=['POST'])
+def izbrisi_proizvod(ID_proizvoda):
+    proizvod = session.query(Proizvod).get(ID_proizvoda)
+    
+    if proizvod:
+        
+        session.delete(proizvod)
+        session.commit()  
+        flash('Proizvod je uspjesno izbrisan.', 'success')
+
+    return redirect(url_for('index'))
+
+
+@app.route('/uredi_proizvod/<int:ID_proizvoda>')
+def uredi_proizvod(ID_proizvoda):
+    proizvod = session.query(Proizvod).get(ID_proizvoda)
+    kategorije = session.query(Kategorija).all()
+    return render_template('uredi_proizvod.html', proizvod=proizvod, kategorije=kategorije)
+
+
+@app.route('/update_proizvod/<int:ID_proizvoda>', methods=['POST'])
+def azuriraj_jelo(ID_proizvoda):
+    proizvod = session.query(Proizvod).get(ID_proizvoda)
+    if proizvod:
+        
+        proizvod.kategorija_id = request.form.get('kategorija_id')
+        proizvod.naziv = request.form.get('naziv')
+        proizvod.opis = request.form.get('opis')
+        
+        proizvod.cijena = request.form.get('cijena')
+        session.commit()
+        
+    
+       
+
+    return redirect(url_for('index'))
+
+
 app.debug = True
 
 if __name__ == '__main__':
